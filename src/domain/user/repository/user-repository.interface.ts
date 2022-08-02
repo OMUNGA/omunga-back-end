82f63c728aa1c9bb7b3621d9f58100592e9676db
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-08-02 12:24:04 +0100

    feat: add interface user

---
+++ src/domain/user/repository/user-repository.interface.ts
@@ -0,0 +1,9 @@
+import RepositoryInterface from "domain/generics/repository/repository-interface";
+import { User } from "../entities/user";
+
+
+
+export default interface UserRepositoryInterface
+    extends RepositoryInterface<User> {
+        Search(entity: string): Promise<void>;
+    }  
