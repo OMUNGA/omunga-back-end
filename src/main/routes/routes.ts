0540297922ffb935c772609de575304897625cc9
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-08-13 23:48:05 +0100

    feat: :sparkles: add all router

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,1 +1,13 @@
-console.log("call all routes here")
+import { UserRouter } from "adapter/user/rest/router/users.router";
+import express from "express";
+import { FactoryUserCreate } from "main/factory/user.factory";
+
+
+const router = express();
+const userController = FactoryUserCreate();
+const userCreateRouter = new UserRouter(router, userController)
+
+
+router.use("/api", userCreateRouter.CreatedUser)
+
+export default router;
