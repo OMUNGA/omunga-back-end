3ae321eeef4132a95790eff3be6a96c3379b092a
Lázaro Manuel <manuelvianacunga@gmail.com>
2022-09-02 13:45:20 +0100

    refactor: :recycle: refatorando ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,16 +1,28 @@
-module.exports = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'ghost',
-  password: 'ghost',
-  database: 'omunga',
-  entities: [
-    './src/infra/database/typeorm/entities/*.ts'],
-  migrations: [
-    './src/infra/database/typeorm/migrations/*.ts'
-  ],
+require("dotenv");
+
+
+const postgres = {
+  type: "postgres",
+  host:
+    process.env.ENVIRONMENT === "local" && process.env.MIGRATION
+      ? "localhost"
+      : process.env.POSTGRES_DB_HOST,
+  port: process.env.POSTGRES_DB_PORT,
+  username: process.env.POSTGRES_DB_USERNAME,
+  password: process.env.POSTGRES_DB_PASSWORD,
+  database: process.env.POSTGRES_DB_DATABASE,
+  entities: [`./src/infra/database/typeorm/entities/*{.ts,.js}`],
+  migrations: [`./src/infra/database/typeorm/migrations/*{.ts,.js}`],
   cli: {
-    migrationsDir: './src/infra/database/typeorm/migrations/'
-  }
+    migrationsDir: './src/infra/database/typeorm/migrations/',
+  },
+};
+
+if (process.env.ENVIRONMENT === "dist") {
+  Object.assign(postgres);
 }
+if (process.env.ENVIRONMENT === "dist-prod") {
+  Object.assign(postgres);
+}
+
+module.exports = postgres;
