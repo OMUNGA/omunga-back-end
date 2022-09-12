16f9101d142b5a05461d4f70e150533e0aec9421
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-09-12 13:14:30 +0100

    refactor: :recycle: refactor typeorm config

--- src/infra/database/connections.ts
+++ src/infra/database/connections.ts
@@ -5,8 +5,8 @@ import { DataSource } from 'typeorm'
 
 export const AppDataSource =  new DataSource({
 	type: 'postgres',
-	host: 'localhost',
-	port: Number(process.env.DB_PORT),
+	host: process.env.DB_HOST,
+	port: Number(process.env.DB_PORT),  
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
