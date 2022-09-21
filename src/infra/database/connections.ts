0b679304dc24c009a41ddb577dc4936d8f3044f8
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-09-21 18:23:50 +0100

    refactor: :recycle: refactor typeorm, migrations and add babbel config to build app

--- src/infra/database/connections.ts
+++ src/infra/database/connections.ts
@@ -2,7 +2,6 @@ import 'dotenv/config'
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 
-
 export const AppDataSource =  new DataSource({
 	type: 'postgres',
 	host: process.env.DB_HOST ,
