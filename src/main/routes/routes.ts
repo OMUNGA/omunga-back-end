e3a9853efe5e8e0c599a68fb6143b0fa3d9bba10
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-15 22:52:35 +0000

    chore: :fire: delete module

--- src/main/routes/routes.ts
+++
@@ -1,7 +0,0 @@
-import { userRouter } from "../../adapter/user/rest/router/user.router";
-import { Router } from "express";
-
-const routes = Router()
-routes.use("/user", userRouter)
-
-export default routes
