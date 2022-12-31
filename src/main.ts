d4c56c646e59901d804b0864b0b8a0fa7a6fc11d
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-12-31 13:49:55 +0100

    add doc api swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { config } from './doc';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
