6e3e29ef78d37222f9b2f88eac774ce1b1b48259
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-12-31 14:01:16 +0100

    add modulo post

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
