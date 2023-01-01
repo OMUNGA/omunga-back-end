7792d2755229acedae08025a72c2f838517d1fd1
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-01-01 16:13:08 +0100

    adicionar modulo comentário

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
