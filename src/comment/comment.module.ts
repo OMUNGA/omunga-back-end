70566b28ffa47af2cef5759d65c009edac8c4a67
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-01-01 16:31:40 +0100

    importar o mudulo prisma

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [CommentController],
-  providers: [CommentService]
+  providers: [CommentService],
+  imports: [PrismaModule],
 })
 export class CommentModule {}
