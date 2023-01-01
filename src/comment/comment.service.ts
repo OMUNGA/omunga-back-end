5df4b5c5b5f9678331284d2359c148c0027d992e
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-01-01 16:36:41 +0100

    crud para comentário regras de negocio sql application

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Injectable()
 export class CommentService {
+
+  constructor(private readonly prisma: PrismaService) { }
+
   create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+    return this.prisma.comment.create({
+      data: createCommentDto
+    })
   }
 
   findAll() {
-    return `This action returns all comment`;
+    return this.prisma.comment.findMany({ where: { deletedAt: false } })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
+  findOne(id: string) {
+    return this.prisma.comment.findUnique({ where: { commentID: id } })
   }
 
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
+  update(id: string, updateCommentDto: UpdateCommentDto) {
+    return this.prisma.comment.update({
+      where: { commentID: id },
+      data: updateCommentDto
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+  remove(id: string) {
+    return this.prisma.comment.update({ where: { commentID: id }, data: { deletedAt: true } });
   }
 }
