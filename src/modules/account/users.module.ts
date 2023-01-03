d02b98021acda48fc3be5f9330f216f02ce0eb7d
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-03 11:06:31 +0100

    refactor: :recycle:  atualizando mdulo de usuário

--- src/modules/account/users.module.ts
+++ src/modules/account/users.module.ts
@@ -2,10 +2,30 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CreateUserService } from './services/create/create-user.service';
 import { CreateUserController } from './controllers/create/create-user.controller';
+import { FindAllUserService } from './services/find-all-users/find-all-users.service';
+import { FindAlllUsersController } from './controllers/find-all-users/find-all-users.controller';
+import { FindOneService } from './services/find-one/find-one-user.service';
+import { DeleteUserService } from './services/delete/delete-user.service';
+import { UpdateUserService } from './services/update/update-user.service';
+import { UpdateUserController } from './controllers/update/update-user.controller';
+import { FindOneController } from './controllers/find-one/find-one-user.controller';
+import { DeleteUserController } from './controllers/delete/delete-user.controller';
 
 @Module({
-  controllers: [CreateUserController],
-  providers: [CreateUserService],
+  controllers: [
+    CreateUserController,
+    FindAlllUsersController,
+    FindOneController,
+    DeleteUserController,
+    UpdateUserController,
+  ],
+  providers: [
+    CreateUserService,
+    FindAllUserService,
+    FindOneService,
+    DeleteUserService,
+    UpdateUserService,
+  ],
   imports: [PrismaModule],
 })
 export class UsersModule {}
