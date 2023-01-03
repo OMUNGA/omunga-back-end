bbe76a0a3e6df1cee95b8bd0650a072b14f7e8e9
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-01-03 03:00:21 +0100

    criar crud em service PostLike

--- src/post-like/post-like.service.ts
+++ src/post-like/post-like.service.ts
@@ -1,26 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePostLikeDto } from './dto/create-post-like.dto';
 import { UpdatePostLikeDto } from './dto/update-post-like.dto';
 
 @Injectable()
 export class PostLikeService {
+
+  constructor(private readonly prisma: PrismaService) { }
+
   create(createPostLikeDto: CreatePostLikeDto) {
-    return 'This action adds a new postLike';
+    return this.prisma.postLike.create({
+      data: createPostLikeDto
+    })
   }
 
   findAll() {
-    return `This action returns all postLike`;
+    return this.prisma.postLike.findMany({ where: { deletedAt: false } })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} postLike`;
+  showAllTheLikesOfThePost(id: string) {
+    return this.prisma.postLike.count({
+      select:{
+        userID: true
+      },
+      where: {
+        postID: id,
+        deletedAt: false 
+      }
+    })
   }
 
-  update(id: number, updatePostLikeDto: UpdatePostLikeDto) {
-    return `This action updates a #${id} postLike`;
+  findOne(id: string) {
+    return this.prisma.postLike.findUnique({ where: { postlikeID: id } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} postLike`;
+  update(id: string, updatePostLikeDto: UpdatePostLikeDto) {
+    return this.prisma.postLike.update({
+      where: { postlikeID: id },
+      data: updatePostLikeDto
+    });
+  }
+
+  remove(id: string) {
+    return this.prisma.postLike.update({ where: { postlikeID: id }, data: { deletedAt: true } });
   }
 }
