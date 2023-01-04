64002bdc798608909523ed9a70f6b52633b97625
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-01-04 23:38:02 +0100

    mover os modulos para pasta modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { CommentLikeModule } from './comment-like/comment-like.module';
+import { CommentLikeModule } from './modules/comment-like/comment-like.module';
 import { CommentModule } from './modules/comment/comment.module';
 import { PostModule } from './modules/post/post.module';
+import { PostLikeModule } from './modules/post-like/post-like.module';
+import { UsersModule } from './modules/account/users.module';
 
 @Module({
-  imports: [PrismaModule, CommentModule, PostModule, CommentLikeModule],
+  imports: [PrismaModule, CommentModule, PostModule, CommentLikeModule,PostLikeModule,UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
