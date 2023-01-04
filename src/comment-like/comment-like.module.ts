3b933e531974151b17bacab7bac161d76dfd3d76
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-01-04 23:31:25 +0100

    import module PrismaModule

--- src/comment-like/comment-like.module.ts
+++ src/comment-like/comment-like.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommentLikeService } from './comment-like.service';
 import { CommentLikeController } from './comment-like.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [CommentLikeController],
-  providers: [CommentLikeService]
+  providers: [CommentLikeService],
+  imports: [PrismaModule],
 })
 export class CommentLikeModule {}
