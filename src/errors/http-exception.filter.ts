e38d92959ed7ecba0c03ab74e4b237f26d34a04e
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-05 13:48:27 +0100

    feat: :sparkles:  add configurantions de erros

---
+++ src/errors/http-exception.filter.ts
@@ -0,0 +1,26 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    console.log('exception', exception);
+
+    response.status(status).json({
+      statusCode: status,
+      message: exception.message,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
