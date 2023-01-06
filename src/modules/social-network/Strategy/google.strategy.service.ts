15abc4ab80c03781b57702581b2340ad95ea226e
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-06 11:00:16 +0100

    feat: :sparkles:  adicioando a estrategia do google relacionando ao login

---
+++ src/modules/social-network/Strategy/google.strategy.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, Profile } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+      scope: ['profile', 'email'],
+    });
+  }
+
+  async validate(accessToken: string, refrashToken: string, profile: Profile) {
+    console.log(accessToken);
+    console.log(refrashToken);
+    console.log(profile);
+  }
+}
