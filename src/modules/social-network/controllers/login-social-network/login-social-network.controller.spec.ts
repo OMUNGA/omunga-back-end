34ce16013b2df29063187d5c213728284434d430
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-06 10:58:03 +0100

    feat: :sparkles:  adicioando o teste do controller de login por redes sociais

---
+++ src/modules/social-network/controllers/login-social-network/login-social-network.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { LoginSocialNetworkController } from './login-social-network.controller';
+
+describe('LoginSocialNetworkController', () => {
+  let controller: LoginSocialNetworkController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [LoginSocialNetworkController],
+    }).compile();
+
+    controller = module.get<LoginSocialNetworkController>(LoginSocialNetworkController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
