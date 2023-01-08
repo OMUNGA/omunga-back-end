9a9a2c328a9a68c9bf01ec75d7943e605758f801
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-08 23:56:17 +0100

    refactor: :recycle: Refatorando main

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { config } from './doc';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const baseURL = process.env.BASE_PATH;
 
   app.setGlobalPrefix(`${baseURL}`);
+  app.use(
+    session({
+      secret: process.env.JWT_SECRET,
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        maxAge: 60000,
+      },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
