a3bdf77002cfdbfc805bb47f6e06fb7933cdeceb
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-08 23:57:39 +0100

    refactor: :recycle: Refatorando modulo de social network

--- src/modules/social-network/social-network.module.ts
+++ src/modules/social-network/social-network.module.ts
@@ -3,10 +3,29 @@ import { LoginSocialNetworkService } from './services/login-social-network/login
 import { LoginSocialNetworkController } from './controllers/login-social-network/login-social-network.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { GoogleStrategy } from './Strategy/google.strategy.service';
+import { LoginService } from '../account/services/login/login.service';
+import { JwtStrategy } from '../account/services/jwt-strategy/jwt.strategy.service';
+import { JwtModule } from '@nestjs/jwt';
+import { SessionSerializer } from './utils/Serializer';
 
 @Module({
-  providers: [LoginSocialNetworkService, GoogleStrategy],
+  providers: [
+    LoginSocialNetworkService,
+    GoogleStrategy,
+    LoginService,
+    JwtStrategy,
+    SessionSerializer,
+  ],
   controllers: [LoginSocialNetworkController],
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      privateKey: process.env.JWT_KEY,
+      secretOrPrivateKey: process.env.JWT_KEY,
+      signOptions: {
+        expiresIn: '2d',
+      },
+    }),
+  ],
 })
 export class SocialNetworkModule {}
