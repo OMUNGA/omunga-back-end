ca9517eee806b907ba54dae98d3cfe9c9f460579
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-08 23:58:55 +0100

    refactor: :recycle: Refatorando estrategia  de login com google

--- src/modules/social-network/Strategy/google.strategy.service.ts
+++ src/modules/social-network/Strategy/google.strategy.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, Profile } from 'passport-google-oauth20';
+import { VerifiedCallback } from 'passport-jwt';
+import { LoginService } from 'src/modules/account/services/login/login.service';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly loginService: LoginService) {
     super({
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
@@ -13,9 +15,32 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(accessToken: string, refrashToken: string, profile: Profile) {
-    console.log(accessToken);
+  async validate(
+    accessToken: string,
+    refrashToken: string,
+    profile: Profile,
+    done: VerifiedCallback,
+  ): Promise<any> {
+    const { name, emails, photos } = profile;
+
+    const user = {
+      email: emails[0].value,
+      name: name.givenName,
+      lastName: name.familyName,
+      picture: photos[0].value,
+      accessToken,
+    };
+
+    done(null, user);
+
+    // const user = await this.loginService.validateUser({
+    //   email: profile.emails[0].value,
+    // });
+
+    // done(null, user);
+
+    console.log('Token', accessToken);
     console.log(refrashToken);
-    console.log(profile);
+    console.log(user);
   }
 }
