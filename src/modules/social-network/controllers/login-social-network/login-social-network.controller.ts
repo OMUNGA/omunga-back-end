ebef3c9bdbc2e1447d5a2d84030c6b741718b3af
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-08 23:58:07 +0100

    refactor: :recycle: Refatorando controller de login google

--- src/modules/social-network/controllers/login-social-network/login-social-network.controller.ts
+++ src/modules/social-network/controllers/login-social-network/login-social-network.controller.ts
@@ -1,9 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Redirect,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { GoogleAuthGuard } from '../../Guard/google-auth.guard';
+import { Request } from 'express';
+import { LoginSocialNetworkService } from '../../services/login-social-network/login-social-network.service';
 
 @Controller('auth')
 export class LoginSocialNetworkController {
+  constructor(private googleLoginService: LoginSocialNetworkService) {}
   @Get('google/login')
-  async handle() {
-    return { msg: 'Google auth' };
+  @UseGuards(GoogleAuthGuard)
+  async googleLogin() {
+    return HttpStatus.OK;
+  }
+
+  @Get('google/redirect')
+  @UseGuards(GoogleAuthGuard)
+  async redirect(@Req() req: Request): Promise<any> {
+    return this.googleLoginService.googleLogin(req);
+  }
+
+  @Get('status')
+  user(@Req() request: Request) {
+    if (request.user) {
+      return { msg: 'logado' };
+    } else {
+      return { msg: 'Not logado' };
+    }
   }
 }
