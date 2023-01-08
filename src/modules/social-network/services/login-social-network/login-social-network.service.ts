7ffc36391972bac63789ca0b2530252186d72ea4
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-08 23:58:45 +0100

    refactor: :recycle: Refatorando servico de login com google

--- src/modules/social-network/services/login-social-network/login-social-network.service.ts
+++ src/modules/social-network/services/login-social-network/login-social-network.service.ts
@@ -4,4 +4,14 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class LoginSocialNetworkService {
   constructor(private readonly primas: PrismaService) {}
+
+  async googleLogin(req: any) {
+    if (!req.user) {
+      return 'No user from google';
+    }
+    return {
+      message: 'User info from google',
+      user: req.user,
+    };
+  }
 }
