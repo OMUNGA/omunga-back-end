ebc8380bde8f4b4df821fd54419bc17801d2de7d
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-10 23:25:01 +0100

    feat: :sparkles: Adicionando    atualização  seguidores services

---
+++ src/modules/followers/services/update/update-followers.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateFollowersDTO } from '../../dtos/update-follower.dto';
+
+@Injectable()
+export class UpdateFollowersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  update(id: string, updateFollowers: UpdateFollowersDTO) {
+    return this.prisma.followers.update({
+      where: { userfollowID: id },
+      data: updateFollowers,
+    });
+  }
+}
