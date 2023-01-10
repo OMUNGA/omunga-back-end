f0ab25c1b37c2231d6954a5de4257b10b5e5eaa2
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-10 22:59:07 +0100

    refactor: :recycle: Refatorando dto

--- src/modules/followers/dtos/add-follower.dto.ts
+++ src/modules/followers/dtos/add-follower.dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class AddFollowersDTO {
+  @IsNotEmpty()
+  @IsString()
   userfollowID: string;
+
+  @IsNotEmpty()
+  @IsString()
   userID: string;
+
   followersID: string;
 }
