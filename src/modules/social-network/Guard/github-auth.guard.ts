8290940723914c3fd22700986f530e71adc1b6a6
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-10 02:02:57 +0100

    feat: :sparkles: Adicionando github guard

---
+++ src/modules/social-network/Guard/github-auth.guard.ts
@@ -0,0 +1,13 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class GithubAuthGuard extends AuthGuard('github') {
+  async canActivate(context: ExecutionContext) {
+    const activate = (await super.canActivate(context)) as boolean;
+    const request = context.switchToHttp().getRequest();
+    await super.logIn(request);
+
+    return activate;
+  }
+}
