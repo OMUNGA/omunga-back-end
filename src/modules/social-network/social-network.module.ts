454c4111b0eb0681b8e827113ad76b889b45fbcd
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-10 01:59:56 +0100

    feat: :sparkles: Adicionando novos modulos

--- src/modules/social-network/social-network.module.ts
+++ src/modules/social-network/social-network.module.ts
@@ -1,22 +1,36 @@
 import { Module } from '@nestjs/common';
-import { LoginSocialNetworkService } from './services/login-social-network/login-social-network.service';
-import { LoginSocialNetworkController } from './controllers/login-social-network/login-social-network.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { GoogleStrategy } from './Strategy/google.strategy.service';
 import { LoginService } from '../account/services/login/login.service';
 import { JwtStrategy } from '../account/services/jwt-strategy/jwt.strategy.service';
 import { JwtModule } from '@nestjs/jwt';
 import { SessionSerializer } from './utils/Serializer';
+import { GoogleAuthController } from './controllers/google-auth/google-auth.controller';
+import { GoogleAuthService } from './services/google-auth/google-auth.service';
+import { GithubStrategy } from './Strategy/github.strategy.service';
+import { GithubAuthService } from './services/github-auth/github-auth.service';
+import { GithubAuthController } from './controllers/github-auth/github-auth.controller';
+import { FacebookStrategy } from './Strategy/facebook.strategy.service';
+import { FacebookAuthController } from './controllers/facebook-auth /facebook-auth.controller';
+import { FacebookAuthService } from './services/facebook-auth /facebook-auth.service';
 
 @Module({
   providers: [
-    LoginSocialNetworkService,
+    GoogleAuthService,
     GoogleStrategy,
+    GithubStrategy,
     LoginService,
     JwtStrategy,
     SessionSerializer,
+    GithubAuthService,
+    FacebookStrategy,
+    FacebookAuthService,
   ],
-  controllers: [LoginSocialNetworkController],
+  controllers: [
+    GoogleAuthController,
+    GithubAuthController,
+    FacebookAuthController,
+  ],
   imports: [
     PrismaModule,
     JwtModule.register({
