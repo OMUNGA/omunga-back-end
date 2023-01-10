2506998c5d10b37417e12214b493d6e4f3e646f6
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-10 22:57:40 +0100

    refactor: :recycle: Refatorando folloer

--- src/modules/followers/followers.module.ts
+++ src/modules/followers/followers.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { CreateFollowersService } from './services/create-followers/create-followers.service';
 import { CreateFollowersController } from './controllers/create-followers/create-followers.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { FindallFollowersController } from './controllers/findAll-followers/findAll-followers.controller';
+import { FindAllFollowersService } from './services/findAll-followers/findAll-followers.service';
 
 @Module({
-  providers: [CreateFollowersService],
-  controllers: [CreateFollowersController],
+  providers: [CreateFollowersService, FindAllFollowersService],
+  controllers: [CreateFollowersController, FindallFollowersController],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      privateKey: process.env.JWT_KEY,
+      secretOrPrivateKey: process.env.JWT_KEY,
+      signOptions: {
+        expiresIn: '2d',
+      },
+    }),
+  ],
 })
 export class FollowersModule {}
