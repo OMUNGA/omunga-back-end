39773f4f689795701fa1403d23f18bcff9d048ee
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-10 22:58:07 +0100

    refactor: :recycle: Refatorando followers controller

--- src/modules/followers/controllers/create-followers/create-followers.controller.ts
+++ src/modules/followers/controllers/create-followers/create-followers.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { AddFollowersDTO } from '../../dtos/add-follower.dto';
+import { CreateFollowersService } from '../../services/create-followers/create-followers.service';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
-@Controller('create-followers')
-export class CreateFollowersController {}
+@Controller('follower')
+export class CreateFollowersController {
+  constructor(private readonly followerService: CreateFollowersService) {}
+
+  @Post('create')
+  @UsePipes(ValidationPipe)
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() followrs: AddFollowersDTO) {
+    return this.followerService.create(followrs);
+  }
+}
