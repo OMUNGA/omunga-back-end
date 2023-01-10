2e17b7fcb2617c4aa1bdf7bca183cc44ad889ce7
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-10 23:23:02 +0100

    refactor: :recycle: Refatorando

--- src/modules/followers/followers.module.ts
+++ src/modules/followers/followers.module.ts
@@ -5,10 +5,24 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { FindallFollowersController } from './controllers/findAll-followers/findAll-followers.controller';
 import { FindAllFollowersService } from './services/findAll-followers/findAll-followers.service';
+import { RemoveFollowerService } from './services/remove/remove-followers.service';
+import { RemoveFollowersController } from './controllers/remove/remove-followers.controller';
+import { UpdateFollowersService } from './services/update/update-followers.service';
+import { UpdateFollowersController } from './controllers/update/update.controller';
 
 @Module({
-  providers: [CreateFollowersService, FindAllFollowersService],
-  controllers: [CreateFollowersController, FindallFollowersController],
+  providers: [
+    CreateFollowersService,
+    FindAllFollowersService,
+    RemoveFollowerService,
+    UpdateFollowersService,
+  ],
+  controllers: [
+    CreateFollowersController,
+    FindallFollowersController,
+    RemoveFollowersController,
+    UpdateFollowersController,
+  ],
   imports: [
     PrismaModule,
     JwtModule.register({
