dc7ea4bd01a9daadffab13cf68fe6a278a77ad48
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-01-10 02:04:38 +0100

    feat: :sparkles: Adicionando google auth service

---
+++ src/modules/social-network/services/google-auth/google-auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class GoogleAuthService {
+  constructor(private readonly primas: PrismaService) {}
+
+  async googleLogin(req: any) {
+    if (!req.user) {
+      return 'No user from google';
+    }
+    return {
+      message: 'User info from google',
+      user: req.user,
+    };
+  }
+}
