48075e53acacb4fe35469c4d097e390c057d318e
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-09 13:30:35 +0100

    refactor: :recycle: Refatorando

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -11,7 +11,7 @@ import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 
-@Controller('comment')
+@Controller('api/v1/comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
