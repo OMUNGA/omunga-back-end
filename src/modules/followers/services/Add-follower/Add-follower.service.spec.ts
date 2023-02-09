83d846f0b40c3981e3e8d389dc9279aa3d9b3f86
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-09 13:09:51 +0100

    refactor: :recycle: Refatorando

--- src/modules/followers/services/Add-follower/Add-follower.service.spec.ts
+++ src/modules/followers/services/Add-follower/Add-follower.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AddFollowerService } from './Add-follower.service';
+import { PrismaModule } from '../../../../prisma/prisma.module';
 
 describe('CreateFollowersService', () => {
   let service: AddFollowerService;
@@ -7,6 +8,7 @@ describe('CreateFollowersService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AddFollowerService],
+      imports: [PrismaModule],
     }).compile();
 
     service = module.get<AddFollowerService>(AddFollowerService);
