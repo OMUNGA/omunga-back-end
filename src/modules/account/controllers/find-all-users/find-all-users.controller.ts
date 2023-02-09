d1794b80fab28b6a5544d5e1bc547a50a9545a4e
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-09 13:30:18 +0100

    refactor: :recycle: Refatorando

--- src/modules/account/controllers/find-all-users/find-all-users.controller.ts
+++ src/modules/account/controllers/find-all-users/find-all-users.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { FindAllUserService } from '../../services/find-all-users/find-all-users.service';
 
-const baseURL = process.env.BASE_PATH;
-@Controller(`${baseURL}/users`)
+@Controller('users')
 export class FindAlllUsersController {
   constructor(private readonly findAllService: FindAllUserService) {}
 
