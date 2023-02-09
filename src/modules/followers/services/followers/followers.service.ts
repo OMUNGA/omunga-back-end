b7af97b31a04803a621f4e815a71ac9f988140b5
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-09 15:30:19 +0100

    refactor: :recycle: Refatorando

--- src/modules/followers/services/followers/followers.service.ts
+++ src/modules/followers/services/followers/followers.service.ts
@@ -6,10 +6,10 @@ export class FollowersService {
   constructor(private readonly prisma: PrismaService) {}
 
   async findAll() {
-    const followers = await this.prisma.followers.findMany({
+    const followers = await this.prisma.follower.findMany({
       where: { deletedAt: false },
     });
-    const countFollowers = await this.prisma.followers.count({
+    const countFollowers = await this.prisma.follower.count({
       where: { deletedAt: false },
     });
 
