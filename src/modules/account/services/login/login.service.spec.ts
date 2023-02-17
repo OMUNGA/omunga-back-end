8fd09a4d0adff65e1ff7ee3f5452a7dabd0b83a3
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:31:30 +0100

    refactor: :recycle: Refatorando

--- src/modules/account/services/login/login.service.spec.ts
+++ src/modules/account/services/login/login.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LoginService } from './login.service';
 
+import { JwtService } from '@nestjs/jwt';
+import { PrismaModule } from '../../../../prisma/prisma.module';
+import { CreateUsersRepository } from '../../repositories/createUserRepository';
+import { PrismaCreateUserRepository } from '../../repositories/implementations/PrismaCreateUserRepository';
+
 describe('LoginService', () => {
   let service: LoginService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LoginService],
+      providers: [
+        LoginService,
+        JwtService,
+        {
+          provide: CreateUsersRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     service = module.get<LoginService>(LoginService);
