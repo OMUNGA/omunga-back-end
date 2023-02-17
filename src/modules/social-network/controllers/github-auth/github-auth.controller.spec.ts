fcd9305348681a8ccb20cb5da3710dca8c1fb6b5
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:51:16 +0100

    refactor: :recycle: Refatorando

--- src/modules/social-network/controllers/github-auth/github-auth.controller.spec.ts
+++ src/modules/social-network/controllers/github-auth/github-auth.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GithubAuthController } from './github-auth.controller';
+import { GithubAuthService } from '../../services/github-auth/github-auth.service';
+import { CreateUsersRepository } from '../../../../modules/account/repositories/createUserRepository';
+import { PrismaCreateUserRepository } from '../../../../modules/account/repositories/implementations/PrismaCreateUserRepository';
+import { PrismaModule } from '../../../../prisma/prisma.module';
 
 describe('LoginSocialNetworkController', () => {
   let controller: GithubAuthController;
@@ -7,6 +11,14 @@ describe('LoginSocialNetworkController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GithubAuthController],
+      providers: [
+        GithubAuthService,
+        {
+          provide: CreateUsersRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     controller = module.get<GithubAuthController>(GithubAuthController);
