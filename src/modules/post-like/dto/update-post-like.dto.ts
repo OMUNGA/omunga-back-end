6490e54e3feadc682a36559791d4f8947361da22
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:49:34 +0100

    refactor: :recycle: Refatorando

--- src/modules/post-like/dto/update-post-like.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePostLikeDto } from './create-post-like.dto';
-
-export class UpdatePostLikeDto extends PartialType(CreatePostLikeDto) {}
