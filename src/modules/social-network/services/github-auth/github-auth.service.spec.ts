9b53f209f951dc8af0b18602a27ad19ce03c6a17
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:51:43 +0100

    refactor: :recycle: Refatorando

--- src/modules/social-network/services/github-auth/github-auth.service.spec.ts
+++ src/modules/social-network/services/github-auth/github-auth.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GithubAuthService } from './github-auth.service';
+import { CreateUsersRepository } from '../../../../modules/account/repositories/createUserRepository';
+import { PrismaModule } from '../../../../prisma/prisma.module';
+import { PrismaCreateUserRepository } from '../../../../modules/account/repositories/implementations/PrismaCreateUserRepository';
 
 describe('LoginSocialNetworkService', () => {
   let service: GithubAuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GithubAuthService],
+      providers: [
+        GithubAuthService,
+        {
+          provide: CreateUsersRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     service = module.get<GithubAuthService>(GithubAuthService);
