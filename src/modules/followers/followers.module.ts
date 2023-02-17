36f054c2267a0ca6a75683ffb2f290058e540349
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:40:07 +0100

    refactor: :recycle: Refatorando

--- src/modules/followers/followers.module.ts
+++ src/modules/followers/followers.module.ts
@@ -1,29 +1,39 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { RemoveFollowerService } from './services/remove/remove-followers.service';
-import { RemoveFollowersController } from './controllers/remove/remove-followers.controller';
-import { UpdateFollowersService } from './services/update/update-followers.service';
-import { UpdateFollowersController } from './controllers/update/update.controller';
-import { FollowersService } from './services/followers/followers.service';
-import { FollowersController } from './controllers/followers/followers.controller';
+
 import { AddFollowerService } from './services/Add-follower/Add-follower.service';
 import { AddFollowerController } from './controllers/Add-follower/Add-follower.controller';
 import { FollowingService } from './services/following/following.service';
 import { FollowingController } from './controllers/following/following.controller';
+import { FollowersRepository } from './repositories/followersRepositories';
+import { PrismaFollowersRepository } from './repositories/implementations/PrismaFollowersRepository';
+import { UnfollowUserController } from './controllers/UnfollowerUser/Unfollower.controller';
+import { UnFollowerService } from './services/unFollower/Unfollower.service';
+import { ShowMyFollowerController } from './controllers/ShowMyfollowers/showMyfollowers.controller';
+import { ShowMyFollowersService } from './services/ShowMyfollowers/showMyfollowers.service';
+import { CreateUsersRepository } from '../account/repositories/createUserRepository';
+import { PrismaCreateUserRepository } from '../account/repositories/implementations/PrismaCreateUserRepository';
 
 @Module({
   providers: [
     AddFollowerService,
-    FollowersService,
-    RemoveFollowerService,
-    UpdateFollowersService,
+    ShowMyFollowersService,
+    UnFollowerService,
     FollowingService,
+
+    {
+      provide: FollowersRepository,
+      useClass: PrismaFollowersRepository,
+    },
+    {
+      provide: CreateUsersRepository,
+      useClass: PrismaCreateUserRepository,
+    },
   ],
   controllers: [
     AddFollowerController,
-    FollowersController,
-    RemoveFollowersController,
-    UpdateFollowersController,
+    ShowMyFollowerController,
+    UnfollowUserController,
     FollowingController,
   ],
   imports: [PrismaModule],
