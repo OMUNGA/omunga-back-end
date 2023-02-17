5a524c7451ee2357af5f4dbda0049755d6a69b7a
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:32:27 +0100

    refactor: :recycle: Refatorando

--- src/modules/account/services/update/update-user.service.ts
+++ src/modules/account/services/update/update-user.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdateUserDTO } from '../../dtos/Update-user.dto ';
+import { CreateUsersRepository } from '../../repositories/createUserRepository';
 
 @Injectable()
 export class UpdateUserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private userRepository: CreateUsersRepository) {}
 
   async update(id: string, updateUserDTO: UpdateUserDTO) {
-    return this.prisma.user.update({
-      where: { userID: id },
-      data: updateUserDTO,
-    });
+    const user = await this.userRepository.update(id, updateUserDTO);
+    return user;
   }
 }
