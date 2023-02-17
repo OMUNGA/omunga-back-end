9ef7abca5c0bda0ebf74eb5296dd57fe4e2532f0
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:40:28 +0100

    refactor: :recycle: Refatorando

--- src/modules/followers/controllers/following/following.controller.spec.ts
+++ src/modules/followers/controllers/following/following.controller.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FollowingController } from './following.controller';
+import { FollowingService } from '../../services/following/following.service';
+import { CreateUsersRepository } from '../../../../modules/account/repositories/createUserRepository';
+import { PrismaCreateUserRepository } from '../../../../modules/account/repositories/implementations/PrismaCreateUserRepository';
+import { FollowersRepository } from '../../repositories/followersRepositories';
+import { PrismaFollowersRepository } from '../../repositories/implementations/PrismaFollowersRepository';
+import { PrismaModule } from '../../../../prisma/prisma.module';
 
 describe('FollowingController', () => {
   let controller: FollowingController;
@@ -7,6 +13,19 @@ describe('FollowingController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FollowingController],
+      providers: [
+        FollowingService,
+
+        {
+          provide: FollowersRepository,
+          useClass: PrismaFollowersRepository,
+        },
+        {
+          provide: CreateUsersRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     controller = module.get<FollowingController>(FollowingController);
