dffa2e2fd336fbd149dde6cb53bb22a5184def09
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:27:43 +0100

    refactor: :recycle: Refatorando

--- src/modules/account/controllers/find-one/find-one-user.controller.ts
+++ src/modules/account/controllers/find-one/find-one-user.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { FindOneService } from '../../services/find-one/find-one-user.service';
+import { AuthUserGuard } from '../../guards/auth.guard';
 
+@UseGuards(AuthUserGuard)
 @Controller(`user`)
 export class FindOneController {
   constructor(private findOneUserService: FindOneService) {}
