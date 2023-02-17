86c0e38551366203071927d570fb7c15e00ce415
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:28:47 +0100

    refactor: :recycle: Refatorando

--- src/modules/account/services/delete/delete-user.service.spec.ts
+++ src/modules/account/services/delete/delete-user.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { DeleteUserService } from './delete.service';
+import { PrismaModule } from '../../../../prisma/prisma.module';
+import { CreateUsersRepository } from '../../repositories/createUserRepository';
+import { DeleteUserService } from './delete-user.service';
+import { PrismaCreateUserRepository } from '../../repositories/implementations/PrismaCreateUserRepository';
 
 describe('DeleteService', () => {
   let service: DeleteUserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DeleteUserService],
+      providers: [
+        DeleteUserService,
+
+        {
+          provide: CreateUsersRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+      ],
+
+      imports: [PrismaModule],
     }).compile();
 
     service = module.get<DeleteUserService>(DeleteUserService);
