0279696581008a711290ee05e62bb79b83d11cc1
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:32:18 +0100

    refactor: :recycle: Refatorando

--- src/modules/account/services/update/update-user.service.spec.ts
+++ src/modules/account/services/update/update-user.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UpdateUserService } from './update-user.service';
+import { PrismaModule } from '../../../../prisma/prisma.module';
+import { CreateUsersRepository } from '../../repositories/createUserRepository';
+import { PrismaCreateUserRepository } from '../../repositories/implementations/PrismaCreateUserRepository';
 
 describe('UpdateService', () => {
   let service: UpdateUserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UpdateUserService],
+      providers: [
+        UpdateUserService,
+        {
+          provide: CreateUsersRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     service = module.get<UpdateUserService>(UpdateUserService);
