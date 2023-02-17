4c5aa8eb3436128c902a3618d9eb651f80f0edf0
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:50:37 +0100

    refactor: :recycle: Refatorando

---
+++ src/modules/post-like/services/remove/remove-post-like.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+import { PostLikeRepository } from '../../repositories/postLikeRepositories';
+
+@Injectable()
+export class RemovePostLikeService {
+  constructor(private readonly postLike: PostLikeRepository) {}
+
+  async remove(id: string) {
+    try {
+      return this.postLike.remove(id);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
