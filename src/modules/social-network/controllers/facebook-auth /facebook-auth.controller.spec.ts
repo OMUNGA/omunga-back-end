68a2d88852d1bc91e9bb473e8f5ab12e73c4d99b
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:51:06 +0100

    refactor: :recycle: Refatorando

--- src/modules/social-network/controllers/facebook-auth /facebook-auth.controller.spec.ts
+++ src/modules/social-network/controllers/facebook-auth /facebook-auth.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FacebookAuthController } from './facebook-auth.controller';
+import { FacebookAuthService } from '../../services/facebook-auth /facebook-auth.service';
+import { CreateUsersRepository } from '../../../../modules/account/repositories/createUserRepository';
+import { PrismaCreateUserRepository } from '../../../../modules/account/repositories/implementations/PrismaCreateUserRepository';
+import { PrismaModule } from '../../../../prisma/prisma.module';
 
 describe('LoginSocialNetworkController', () => {
   let controller: FacebookAuthController;
@@ -7,6 +11,14 @@ describe('LoginSocialNetworkController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FacebookAuthController],
+      providers: [
+        FacebookAuthService,
+        {
+          provide: CreateUsersRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     controller = module.get<FacebookAuthController>(FacebookAuthController);
