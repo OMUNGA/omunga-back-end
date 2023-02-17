4dedc6df25ad2f1e9c6712ec40276578a012c515
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:43:01 +0100

    refactor: :recycle: Refatorando

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,11 +1,48 @@
 import { Module } from '@nestjs/common';
-import { PostService } from './post.service';
-import { PostController } from './post.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { CreatePostController } from './controllers/create/create-post.controller';
+import { FindAllPostController } from './controllers/findAll/findAll-post.controller';
+import { FindOnePostController } from './controllers/findOne/post.controller';
+import { SearchPostController } from './controllers/search/search-post.controller';
+import { UpdatePostController } from './controllers/update/update-post.controller';
+import { RemovePostController } from './controllers/remove/remove-post.controller';
+import { CreatePostService } from './services/create/create-post.service';
+import { UpdatePostService } from './services/update/update-post.service';
+import { FindOnePostService } from './services/findOne/findOne-post.service';
+import { FindAllPostService } from './services/findAll/findAll-post.service';
+import { RemovePostService } from './services/remove/remove-post.service';
+import { SearchPostService } from './services/search/search-post.service';
+import { PostRepository } from './repositories/postRepositories';
+import { PrismaPostRepository } from './repositories/implementations/prismaPostRepositories';
+import { CreateUsersRepository } from '../account/repositories/createUserRepository';
+import { PrismaCreateUserRepository } from '../account/repositories/implementations/PrismaCreateUserRepository';
 
 @Module({
-  controllers: [PostController],
-  providers: [PostService],
+  controllers: [
+    CreatePostController,
+    FindAllPostController,
+    FindOnePostController,
+    SearchPostController,
+    UpdatePostController,
+    RemovePostController,
+  ],
+  providers: [
+    CreatePostService,
+    UpdatePostService,
+    FindOnePostService,
+    FindAllPostService,
+    RemovePostService,
+    SearchPostService,
+
+    {
+      provide: PostRepository,
+      useClass: PrismaPostRepository,
+    },
+    {
+      provide: CreateUsersRepository,
+      useClass: PrismaCreateUserRepository,
+    },
+  ],
   imports: [PrismaModule],
 })
 export class PostModule {}
