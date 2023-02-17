6bf0ab58064afcc4f9a91d2690bf597a9d0d334a
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:28:06 +0100

    refactor: :recycle: Refatorando

--- src/modules/account/controllers/update/update-user.controller.spec.ts
+++ src/modules/account/controllers/update/update-user.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UpdateUserController } from './update-user.controller';
+import { UpdateUserService } from '../../services/update/update-user.service';
+import { PrismaModule } from '../../../../prisma/prisma.module';
+import { CreateUsersRepository } from '../../repositories/createUserRepository';
+import { PrismaCreateUserRepository } from '../../repositories/implementations/PrismaCreateUserRepository';
 
 describe('UpdateController', () => {
   let controller: UpdateUserController;
@@ -7,6 +11,14 @@ describe('UpdateController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UpdateUserController],
+      providers: [
+        UpdateUserService,
+        {
+          provide: CreateUsersRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     controller = module.get<UpdateUserController>(UpdateUserController);
