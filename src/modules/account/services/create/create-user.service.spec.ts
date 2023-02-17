fa74aa41bb15b64749a545296e4bc8c17717e0dc
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-17 10:28:38 +0100

    refactor: :recycle: Refatorando

--- src/modules/account/services/create/create-user.service.spec.ts
+++ src/modules/account/services/create/create-user.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateUserService } from './create-user.service';
+import { CreateUsersRepository } from '../../repositories/createUserRepository';
+import { PrismaCreateUserRepository } from '../../repositories/implementations/PrismaCreateUserRepository';
+import { PrismaModule } from '../../../../prisma/prisma.module';
 
 describe('CreateUserServiceService', () => {
   let service: CreateUserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CreateUserService],
+      providers: [
+        CreateUserService,
+        {
+          provide: CreateUsersRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     service = module.get<CreateUserService>(CreateUserService);
