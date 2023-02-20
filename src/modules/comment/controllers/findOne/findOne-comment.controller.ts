3be42e1d9957400693ad36d180f510d10b27b7ff
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-20 23:05:20 +0100

    refactor: :recycle: refatorando e adicionano ultimos ajutes

--- src/modules/comment/controllers/findOne/findOne-comment.controller.ts
+++ src/modules/comment/controllers/findOne/findOne-comment.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { AuthUserGuard } from '../../../../modules/account/guards/auth.guard';
-import { FindOneService } from '../../../../modules/account/services/find-one/find-one-user.service';
+import { FindOneCommentService } from '../../services/findOne/findOne-comment.service';
 
 @UseGuards(AuthUserGuard)
 @Controller('comment')
 export class FindOneCommentController {
-  constructor(private readonly commentService: FindOneService) {}
+  constructor(private readonly commentService: FindOneCommentService) {}
 
   @Get(':id')
   findOne(@Param('id') id: string) {
