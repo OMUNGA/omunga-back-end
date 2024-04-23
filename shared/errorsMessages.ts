6bf996cf5d5052af600e47f358e3404889a34a05
manuelcunga <manuelvianacunga@gmail.com>
2024-04-24 00:15:11 +0100

    feat: :sparkles: adding enum for erros

---
+++ shared/errorsMessages.ts
@@ -0,0 +1,8 @@
+export enum messages {
+  Unauthorized = 'Email ou palavra-passe errada!',
+  Unauthenticated = 'Você precisa estar autenticado!',
+  NotFoundUser = 'Usuário não encontrado',
+  InternalServerError = "Ocorreu um erro interno. Por favor, tente novamente mais tarde.",
+  BadRequestError = "Ups, este usuário já existe!",
+  AlreadyFollowing = "Ups, você já esta seguindo este usuário"
+}
